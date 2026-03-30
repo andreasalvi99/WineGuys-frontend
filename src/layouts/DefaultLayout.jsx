6e29e38bcfa8ee81b47b60ff09318f1dbf475792
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-03-30 14:56:22 +0200

    add outlet

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,9 +1,15 @@
+import { Outlet } from "react-router-dom";
 import Navbar from "../../components/Navbar";
 
 export default function DefaultLayout() {
   return (
     <>
       <Navbar />
+      <section>
+        <div className="container text-center">
+          <Outlet />
+        </div>
+      </section>
     </>
   );
 }
