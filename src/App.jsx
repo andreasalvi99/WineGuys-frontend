1b817a12176f2856c41a578c9cabd86d85d026de
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-03-30 16:07:22 +0200

    change layout destination

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
-import defaultLayout from "./layouts/DefaultLayout";
+import DefaultLayout from "../layouts/DefaultLayout";
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route Component={defaultLayout}>
+          <Route Component={DefaultLayout}>
             <Route index Component={HomePage} />
             {/* <Route path="/vini" Component={WinesPage} />
           <Route path="/vini/:id" Component={WinesDetailPage} /> */}
