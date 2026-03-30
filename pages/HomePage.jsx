f22827526a4fac2a8326f3dce5d9fbdf4ba5c2c0
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-03-30 15:21:27 +0200

    add description about us

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -1,3 +1,24 @@
 export default function HomePage() {
-  return <>Salve</>;
+  return (
+    <>
+      <p>
+        <em>
+          "WineGuys è una giovane realtà nata dall’idea di cinque amici uniti da
+          una passione comune: il vino. Quello vero, non quello scelto a caso al
+          supermercato perché “boh, l’etichetta è carina”. L’obiettivo è
+          semplice: rendere il mondo del vino più accessibile, senza snobismi
+          inutili e senza dover avere un sommelier interiore per scegliere una
+          bottiglia decente. Attraverso il loro e-commerce, WineGuys seleziona
+          vini di qualità provenienti da diverse regioni, dando spazio sia a
+          cantine affermate che a piccoli produttori che meritano di essere
+          scoperti. Ogni prodotto è accompagnato da descrizioni chiare e
+          consigli pratici, pensati per aiutare chiunque, dal curioso al più
+          esperto, a trovare il vino giusto per ogni occasione. WineGuys non è
+          solo vendita online, ma un progetto che punta a costruire una
+          community di appassionati, dove il vino diventa un’esperienza da
+          condividere, senza complicazioni e senza pretese."
+        </em>
+      </p>
+    </>
+  );
 }
