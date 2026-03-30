b9a5580f5092e122075096b02652141cfb71e459
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-03-30 14:55:26 +0200

    add navbar components, default layout page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
+import defaultLayout from "./layouts/DefaultLayout";
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route index Component={HomePage} />
-          <Route path="/vini" Component={WinesPage} />
-          <Route path="/vini/:id" Component={WinesDetailPage} />
+          <Route Component={defaultLayout}>
+            <Route index Component={HomePage} />
+            {/* <Route path="/vini" Component={WinesPage} />
+          <Route path="/vini/:id" Component={WinesDetailPage} /> */}
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
