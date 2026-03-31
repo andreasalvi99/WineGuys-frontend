389eaa469e3138a2745a0e81ec6e1bc25ff11ef2
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-03-31 12:44:58 +0200

    print wine card for test

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -1,29 +1,36 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function HomePage() {
+  const [bestSeller, setBestSeller] = useState([]);
+
+  function fetchBestSeller() {
+    axios.get("http://localhost:3000/vini/promo").then((response) => {
+      console.log(response.data);
+      setBestSeller(response.data);
+    });
+  }
+
+  useEffect(fetchBestSeller, []);
+
   return (
     <>
       <section id="primary" className="">
         <div className="container text-center">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe
-          maxime, atque quaerat cumque accusantium eaque. Ipsa earum sit
-          consectetur rem? Eligendi non pariatur esse debitis autem molestiae
-          quaerat at architecto? Dolores quis repudiandae commodi eligendi
-          mollitia tempora totam, illum esse blanditiis. Molestiae ad expedita
-          culpa obcaecati, cumque modi rem hic dolor laudantium, eius debitis
-          doloremque, amet iusto voluptate illum adipisci! Quasi perspiciatis
-          assumenda facilis voluptate voluptatem eaque maiores enim minima fuga
-          vero ipsa voluptas ullam non nemo sequi unde placeat laboriosam,
-          labore obcaecati. Voluptatem facilis at debitis fuga, aspernatur
-          asperiores. Delectus aliquid qui pariatur tenetur impedit iste
-          voluptatem, eaque repellat consectetur fuga officiis inventore harum
-          iure consequuntur quidem saepe. Magni repudiandae ea deleniti cumque
-          ipsa ipsum voluptas. Cumque, dolor impedit. Dolorum aspernatur
-          distinctio numquam cum aperiam incidunt fugit, labore error! Debitis
-          aperiam inventore voluptates vero mollitia blanditiis velit laborum
-          similique molestias dolor est, eum aliquid sunt numquam quibusdam ab
-          commodi. Esse corporis fugit odit nemo autem est sequi exercitationem
-          consequuntur praesentium ab id dignissimos nulla illo laborum, eveniet
-          quae. Ipsam veritatis eveniet eum ea nobis earum doloremque eos
-          tempore velit?
+          {bestSeller.map((item) => {
+            return (
+              <div key={item.id} className="card">
+                <img
+                  src={item.img}
+                  className="card-img-top"
+                  alt={item.product_name}
+                />
+                <div className="card-body">
+                  <p className="card-text">{item.description}</p>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </section>
       <div className="container text-center">
