23cd9fdaaee27dcc440a12cedecceb1ceca55937
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-03-31 21:24:41 +0200

    add logo next to chi siamo

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -41,30 +41,42 @@ export default function HomePage() {
     <>
       <section className="playfair-display_special">
         <div className="container text-start">
-          <div className="d-flex justify-content-start">
-            <div>
-              <h1 className="border-bottom border-black">CHI SIAMO</h1>
+          <div className="d-flex justify-content-between">
+            <div className="">
+              <div className="d-flex justify-content-start">
+                <div>
+                  <h1 className="border-bottom border-black">CHI SIAMO</h1>
+                </div>
+              </div>
+              <p className="fs-5 my-3">
+                <em>
+                  "WineGuys è una giovane realtà nata dall’idea di cinque amici
+                  uniti da una passione comune: il vino. Quello vero, non quello
+                  scelto a caso al supermercato perché “boh, l’etichetta è
+                  carina”. L’obiettivo è semplice: rendere il mondo del vino più
+                  accessibile, senza snobismi inutili e senza dover avere un
+                  sommelier interiore per scegliere una bottiglia decente.
+                  Attraverso il nostro e-commerce, selezioniamo vini di qualità
+                  provenienti da diverse regioni, dando spazio sia a cantine
+                  affermate che a piccoli produttori che meritano di essere
+                  scoperti. Ogni prodotto è accompagnato da descrizioni chiare e
+                  consigli pratici, pensati per aiutare chiunque, dal curioso al
+                  più esperto, a trovare il vino giusto per ogni occasione.
+                  WineGuys non è solo vendita online, ma un progetto che punta a
+                  costruire una community di appassionati, dove il vino diventa
+                  un’esperienza da condividere, senza complicazioni e senza
+                  pretese."
+                </em>
+              </p>
+            </div>
+            <div className="">
+              <img
+                src="../src/assets/img/wineguys_logo2.png"
+                alt=""
+                className="secondary-logo"
+              />
             </div>
           </div>
-          <p className="fs-5 my-3">
-            <em>
-              "WineGuys è una giovane realtà nata dall’idea di cinque amici
-              uniti da una passione comune: il vino. Quello vero, non quello
-              scelto a caso al supermercato perché “boh, l’etichetta è carina”.
-              L’obiettivo è semplice: rendere il mondo del vino più accessibile,
-              senza snobismi inutili e senza dover avere un sommelier interiore
-              per scegliere una bottiglia decente. Attraverso il nostro
-              e-commerce, selezioniamo vini di qualità provenienti da diverse
-              regioni, dando spazio sia a cantine affermate che a piccoli
-              produttori che meritano di essere scoperti. Ogni prodotto è
-              accompagnato da descrizioni chiare e consigli pratici, pensati per
-              aiutare chiunque, dal curioso al più esperto, a trovare il vino
-              giusto per ogni occasione. WineGuys non è solo vendita online, ma
-              un progetto che punta a costruire una community di appassionati,
-              dove il vino diventa un’esperienza da condividere, senza
-              complicazioni e senza pretese."
-            </em>
-          </p>
         </div>
       </section>
       <section id="promos" className="">
