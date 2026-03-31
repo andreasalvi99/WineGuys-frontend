56c5f1a4bd27d43e575dafdab6c28c231abd0c4d
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-03-31 14:29:51 +0200

    add scale effect when hover

--- components/WineCard.jsx
+++ components/WineCard.jsx
@@ -1,7 +1,7 @@
 export default function WineCard({ img, name, price }) {
   return (
     <div className="col">
-      <div className="card h-100">
+      <div className="card h-100 bg-transparent border-0">
         <img src={img} className="card-img-top" alt={name} />
         <div className="card-body">
           <p className="card-text">{name}</p>
