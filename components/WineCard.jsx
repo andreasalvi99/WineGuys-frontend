5d3c4a28c27d5ee5ec473af76282e89381aaf141
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-03-31 14:35:13 +0200

    add Link

--- components/WineCard.jsx
+++ components/WineCard.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function WineCard({ img, name, price }) {
   return (
     <div className="col">
       <div className="card h-100 bg-transparent border-0">
-        <img src={img} className="card-img-top" alt={name} />
+        <Link to={"/vini:slug"}>
+          <img src={img} className="card-img-top" alt={name} />
+        </Link>
         <div className="card-body">
           <p className="card-text">{name}</p>
           <p className="card-text">{price} &euro;</p>
