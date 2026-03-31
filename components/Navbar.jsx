1f44880bb5397552c160867ba3007d8cf08ce553
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-03-31 22:13:14 +0200

    fix navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,9 +10,7 @@ export default function Navbar() {
       </div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar
-          </a>
+          <a className="navbar-brand" href="#"></a>
           <button
             className="navbar-toggler"
             type="button"
@@ -24,8 +22,11 @@ export default function Navbar() {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <div
+            className="collapse navbar-collapse justify-content-lg-center"
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav mb-2 mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#">
                   Home
