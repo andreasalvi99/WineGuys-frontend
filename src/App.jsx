1718371602b37ca08ef8349fb7d1780da22a4370
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-04-01 10:32:11 +0200

    correction pages import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
-import WineDetailPage from "./pages/WineDetailPage";
-import DefaultLayout from "./layouts/DefaultLayout";
+import WineDetailPage from "../pages/WineDetailPage";
+import DefaultLayout from "../layouts/DefaultLayout";
 export default function App() {
   return (
     <>
