7e8112966484a5ea1d6b30079b425f7c00598e6a
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-04-02 20:06:18 +0200

    map card of cart items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,9 @@ import { Link, NavLink } from "react-router-dom";
 import { CartContext } from "../context/CartContextObject";
 
 export default function Navbar() {
-  const { cart, addToCart } = useContext(CartContext);
+  const { cart } = useContext(CartContext);
+
+  console.log(cart);
 
   return (
     <>
@@ -88,7 +90,38 @@ export default function Navbar() {
               aria-label="Close"
             ></button>
           </div>
-          <div className="offcanvas-body">...</div>
+          <div className="offcanvas-body">
+            {cart.map((item) => {
+              return (
+                <div className="card mb-3">
+                  <div className="row g-0">
+                    <div className="col-md-4">
+                      <img
+                        src={`http://localhost:3000/wines/${item.img}`}
+                        className="img-fluid rounded-start"
+                        alt="..."
+                      />
+                    </div>
+                    <div className="col-md-8">
+                      <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">
+                          This is a wider card with supporting text below as a
+                          natural lead-in to additional content. This content is
+                          a little bit longer.
+                        </p>
+                        <p className="card-text">
+                          <small className="text-body-secondary">
+                            Last updated 3 mins ago
+                          </small>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </>
