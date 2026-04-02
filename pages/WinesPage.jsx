7a1b344dfe343d3ff8a64044c6e565a65856461c
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-04-02 11:21:42 +0200

    add map for wines

--- pages/WinesPage.jsx
+++ pages/WinesPage.jsx
@@ -1,3 +1,37 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function WinesPage() {
-  return <>Ciaoo</>;
+  const [wines, setWines] = useState([]);
+
+  function fetchWines() {
+    axios.get("http://localhost:3000/vini").then((response) => {
+      console.log(response.data);
+      setWines(response.data);
+    });
+  }
+
+  useEffect(fetchWines, []);
+
+  return (
+    <>
+      <div className="row row-cols-4">
+        {wines.map((wine) => {
+          return (
+            <div className="col">
+              <div className="card">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card’s content.
+                  </p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
