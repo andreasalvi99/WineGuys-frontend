194c47bb46cd03d161d5ec9c4866fd7e8a3e42ab
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-04-02 22:16:54 +0200

    create another wine card for vini page

---
+++ src/components/AllWinesCard.jsx
@@ -0,0 +1,69 @@
+import { Link } from "react-router-dom";
+import { CartContext } from "../context/CartContextObject";
+import { useContext } from "react";
+
+export default function AllWinesCard({
+  wine,
+  slug,
+  promotion,
+  price,
+  quantity,
+  img,
+  name,
+  calcDiscount,
+}) {
+  const { addToCart } = useContext(CartContext);
+  return (
+    <div className="col">
+      <div className="card h-100 p-3 wine-card bg-body-tertiary">
+        <Link to={"/vini/" + slug} className="text-black text-decoration-none">
+          <div className="d-flex justify-content-between">
+            <div>
+              {promotion != null && (
+                <span className="badge text-bg-danger fs-6">
+                  -{calcDiscount(price, promotion)}%
+                </span>
+              )}
+
+              {quantity > 0 && quantity <= 6 && (
+                <span className="text-danger">Ne rimangono {quantity}</span>
+              )}
+
+              {quantity === 0 && <span className="text-danger">Esaurito</span>}
+            </div>
+            <div>
+              {promotion !== null && promotion !== undefined ? (
+                <>
+                  <p className="text-danger m-0">&euro;{promotion}</p>
+                  <p className="text-decoration-line-through m-0">
+                    &euro;
+                    {price}
+                  </p>
+                </>
+              ) : (
+                <p>&euro;{price}</p>
+              )}
+            </div>
+          </div>
+          <img src={img} className="card-img-top" alt={name} />
+        </Link>
+        <div className="card-body text-center d-flex flex-column justify-content-between">
+          <p className="card-text">{name}</p>
+          {quantity > 0 ? (
+            <button
+              onClick={() => addToCart(wine, 1)}
+              type="button"
+              className="btn btn-outline-danger"
+            >
+              Aggiungi al carrello
+            </button>
+          ) : (
+            <button type="button" className="btn btn-outline-danger" disabled>
+              Aggiungi al carrello
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
