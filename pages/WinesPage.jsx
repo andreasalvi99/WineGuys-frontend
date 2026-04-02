06d2724c90db7d71107b2f6ba512f19a6b389323
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-04-02 12:19:04 +0200

    add layover and add to cart btn

--- pages/WinesPage.jsx
+++ pages/WinesPage.jsx
@@ -26,6 +26,11 @@ export default function WinesPage() {
               return (
                 <div key={wine.id} className="col">
                   <div className="card h-100 p-3 wine-card bg-body-tertiary">
+                    <div className="layover">
+                      <button type="button" className="btn btn-outline-danger">
+                        Aggiungi al carrello
+                      </button>
+                    </div>
                     <Link to={"/vini/" + wine.slug}>
                       <img
                         src={`http://localhost:3000/wines/${wine.img}`}
