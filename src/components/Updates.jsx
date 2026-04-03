9fb46df1e524cd37299e9a79cff0c25b09719597
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-04-03 15:08:45 +0200

    change class into class name

--- src/components/Updates.jsx
+++ src/components/Updates.jsx
@@ -3,7 +3,7 @@ export default function Updates() {
     <section>
       <div className="container text-center">
         <p className="blink">
-          <i class="bi bi-truck mx-2 shipping-icon"></i>
+          <i className="bi bi-truck mx-2 shipping-icon"></i>
           Spedizione gratuita per ordini superiori a &euro;60
         </p>
       </div>
