309e5e5ef5a59bbe56468974ca015735dd095ec9
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-04-07 12:56:40 +0200

    implemented nto found page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -3,11 +3,17 @@ import { Link } from "react-router-dom";
 export default function NotFoundPage() {
   return (
     <>
-      <div className="container d-flex justify-content-center align-items-center">
-        <Link to={"/"}>
-          <button className="btn btn-outline-dark">Torna alla Home</button>
-        </Link>
-      </div>
+      <section className="not-found p-5">
+        <div className="container d-flex flex-column justify-content-center align-items-center playfair-display_special p-5 mt-5">
+          <h1 className="search-outcome text-center mt-5">
+            Oops! sembrerebbe che la tua ricerca di vini non sia andata a buon
+            fine
+          </h1>
+          <Link to={"/"}>
+            <button className="btn btn-dark mt-4">Torna alla Home</button>
+          </Link>
+        </div>
+      </section>
     </>
   );
 }
