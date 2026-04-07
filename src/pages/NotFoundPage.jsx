def491a460a748f7e8ae1a7efd0b77c4fd554491
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-04-07 15:52:38 +0200

    change not found page layout

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -3,15 +3,17 @@ import { Link } from "react-router-dom";
 export default function NotFoundPage() {
   return (
     <>
-      <section className="not-found p-5">
-        <div className="container d-flex flex-column justify-content-center align-items-center playfair-display_special p-5 mt-5">
-          <h1 className="search-outcome text-center mt-5">
-            Oops! sembrerebbe che la tua ricerca di vini non sia andata a buon
-            fine
-          </h1>
-          <Link to={"/"}>
-            <button className="btn btn-dark mt-4">Torna alla Home</button>
-          </Link>
+      <section className="p-5">
+        <div className="container text-center d-flex justify-content-center mt-5 playfair-display_special">
+          <div className="not-found d-flex justify-content-center align-items-center flex-column">
+            <p className="text-white h2">
+              Oops! sembrerebbe che la tua ricerca di vini non sia andata a buon
+              fine
+            </p>
+            <Link to={"/"}>
+              <button className="btn btn-dark">Torna alla Home</button>
+            </Link>
+          </div>
         </div>
       </section>
     </>
