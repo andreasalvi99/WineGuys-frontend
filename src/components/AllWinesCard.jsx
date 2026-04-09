c0fb25166fffea9b5ab93d7b6c1f49c429ffc81e
AntonioCappellano <antoniocappellano97@gmail.com>
2026-04-09 16:07:15 +0200

    add toast into AllWinesCard

--- src/components/AllWinesCard.jsx
+++ src/components/AllWinesCard.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { CartContext } from "../context/CartContextObject";
 import { useContext } from "react";
+import { toast } from "sonner";
 
 export default function AllWinesCard({
   wine,
@@ -13,29 +14,36 @@ export default function AllWinesCard({
   calcDiscount,
 }) {
   const { addToCart, cart } = useContext(CartContext);
+
+  const handleAddToCart = () => {
+    addToCart(wine, 1);
+    
+    // Mostra il toast
+    toast.success("Aggiunto al carrello", {
+      description: `${name} è stato aggiunto al carrello`,
+      duration: 2000,
+    });
+  };
+
   return (
     <div className="col">
       <div className="card h-100 p-3 wine-card bg-body-tertiary">
         <Link to={"/vini/" + slug} className="text-black text-decoration-none">
           <div className="d-flex justify-content-between">
-            {/* se chiave promotion ha un valore allora invoco la funzione per calcolare lo sconto e lo stampo in uno span */}
             <div>
               {promotion != null && (
                 <span className="badge text-bg-danger fs-6 mb-2">
                   -{calcDiscount(price, promotion)}%
                 </span>
               )}
-              {/* se la quantity in stock del prodotto è compresa tra 0 e 6 appare un avviso lampeggiante specificando la quantità rimanente */}
               {quantity > 0 && quantity <= 6 && (
                 <span className="text-danger blink d-block">
                   <div className="red-pin bg-danger"></div>
                   {quantity} rimamenti
                 </span>
               )}
-              {/* se la quantity in stock è uguale a 0 appare scritto esaurito */}
               {quantity === 0 && <span className="text-danger">Esaurito</span>}
             </div>
-            {/* se  chiave promotion ha un valore allora stampo il valore originale sbarrato e sotto il valore scontato altrimenti stampo il prezzo originale*/}
             <div>
               {promotion !== null && promotion !== undefined ? (
                 <>
@@ -43,8 +51,7 @@ export default function AllWinesCard({
                     &euro;{promotion}
                   </p>
                   <p className="text-decoration-line-through m-0">
-                    &euro;
-                    {price}
+                    &euro;{price}
                   </p>
                 </>
               ) : (
@@ -56,9 +63,9 @@ export default function AllWinesCard({
         </Link>
         <div className="card-body text-center d-flex flex-column justify-content-between">
           <p className="card-text">{name}</p>
-          {/* il button aggiungi al carello è disabilitato se quantity in stock è 0 o se  la quantità che si sta aggiungendo al carrello è maggiore di quella in stock*/}
+          
           <button
-            onClick={() => addToCart(wine, 1)}
+            onClick={handleAddToCart}
             type="button"
             className="btn btn-outline-danger"
             disabled={
